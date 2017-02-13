0eca88fe86daa2b909b1ae69523d81f32276b95e
mohammad4x <m.ferdosi94@gmail.com>
2017-02-13 21:18:59 +0330

    Implement Set Message Model Action

---
+++ actions/message/setMessageModelAction.js
@@ -0,0 +1,27 @@
+var setSubscribeModelLogic = require('../../logic/subscribe/setSubscribeModelLogic')
+
+var Input = {
+	token: {
+		required: true
+	}
+}
+
+exports.setSubscribeModelAction = {
+	name: 'setSubscribeModelAction',
+	description: 'Set Subscribe Model',
+	inputs: Input,
+	
+	run: function (api, data, next) {
+		var payload = JSON.parse(JSON.stringify(data.connection.rawConnection.params.body))
+		setSubscribeModelLogic.setSubscriberModel(data.params.token, payload, function (error, replies) {
+			if (error) {
+				data.response.error = error.error
+				next(error)
+			}
+			else {
+				data.response.result = replies
+				next()
+			}
+		})
+	}
+}
