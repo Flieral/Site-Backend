b45627351c9c2b4c87c8cc9532e80138440fbfec
mohammad4x <m.ferdosi94@gmail.com>
2017-02-13 21:19:12 +0330

    Implement Get Subscribe Model Action

---
+++ actions/subscribe/getSubscribeModelAction.js
@@ -0,0 +1,17 @@
+var getSubscribeModelLogic = require('../../logic/subscribe/getSubscribeModelLogic')
+
+var Input = {
+	Token: {
+		required: true
+	}
+}
+
+exports.getSubscribeModelAction = {
+	name: 'getSubscribeModelAction',
+	description: 'Get Subscribe Model',
+	inputs: Input,
+	
+	run: function (api, data, next) {
+		
+	}
+}
