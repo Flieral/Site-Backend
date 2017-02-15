85b8b4ced7e25e53c98b82d9ba0975c2ed6dad41
MrWooJ <hurricanc@gmail.com>
2017-02-15 16:13:39 +0330

    Fix Second I/O Request Issue

--- logic/message/setMessageModelLogic.js
+++ logic/message/setMessageModelLogic.js
@@ -1,80 +1,90 @@
 var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'adp',
-  database: 'FlieralSite'
-})
 
 module.exports = {
   setMessageModel: function (token, payload, callback) {
-    connection.connect()
-    connection.query('SELECT * FROM ArrivedMessagesToken WHERE Token =?', token, function (error, results) {
-      if (error) {
-        callback(error, null)
-        connection.end()
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'adp',
+      database: 'FlieralSite'
+    })
+    connection.connect(function (err) {
+      if (err) {
+        callback(err, null)
         return
       }
-      if (results[0].Token !== null && results[0].Counter < 5) {
-        var newCounter = results[0].Counter + 1
-        connection.query('UPDATE ArrivedMessagesToken SET Counter = ? WHERE Token = ?', [newCounter, token], function (error, results2) {
-          if (error) {
-            callback(error, null)
-            connection.end()
-            return
-          }
-          var newRowtoMsginUpdate = {
-            Name: payload.name,
-            Email: payload.email,
-            Phone: payload.phone,
-            Subject: payload.subject,
-            Message: payload.message,
-            TokenId: token
-          }
-          connection.query('INSERT INTO ArrivedMessages SET ', newRowtoMsginUpdate, function (error, results3) {
+      connection.query('SELECT * FROM ArrivedMessagesToken WHERE Token =?', token, function (error, results) {
+        if (error) {
+          callback(error, null)
+          connection.end()
+          return
+        }
+        if ((results.length != 0) && results[0].Token !== null && parseInt(results[0].Counter, 10) < 5) {
+          var newCounter = parseInt(results[0].Counter, 10) + 1
+          connection.query('UPDATE ArrivedMessagesToken SET Counter = ? WHERE Token = ?', [newCounter, token], function (error, results2) {
             if (error) {
               callback(error, null)
               connection.end()
               return
             }
-            callback(null, results3.affectedRows)
-            connection.end()
-            return
-          })
-        })
-      } else {
-        if (results[0].Token === null) {
-          var newRowtoMsgToken = {
-            Token: token,
-            Counter: 1
-          }
-          connection.query('INSERT INTO ArrivedMessagesToken SET ', newRowtoMsgToken, function (error, results4) {
-            if (error) {
-              callback(error, null)
-              connection.end()
-              return
-            }
-            var newRowtoMsginInsert = {
+            var newRowtoMsginUpdate = {
               Name: payload.name,
               Email: payload.email,
               Phone: payload.phone,
               Subject: payload.subject,
               Message: payload.message,
-              TokenId: results4.insertId
+              TokenId: token
             }
-            connection.query('INSERT INTO ArrivedMessages SET ', newRowtoMsginInsert, function (error, results5) {
+            connection.query('INSERT INTO ArrivedMessages SET ', newRowtoMsginUpdate, function (error, results3) {
               if (error) {
                 callback(error, null)
                 connection.end()
                 return
               }
-              callback(null, results5.affectedRows)
+              callback(null, results3.affectedRows)
               connection.end()
               return
             })
           })
+        } else {
+          if (results.length == 0) {
+            var newRowtoMsgToken = {
+              Token: token,
+              Counter: 1
+            }
+            connection.query('INSERT INTO ArrivedMessagesToken SET ', newRowtoMsgToken, function (error, results4) {
+              if (error) {
+                callback(error, null)
+                connection.end()
+                return
+              }
+              var newRowtoMsginInsert = {
+                Name: payload.name,
+                Email: payload.email,
+                Phone: payload.phone,
+                Subject: payload.subject,
+                Message: payload.message,
+                TokenId: results4.insertId
+              }
+              connection.query('INSERT INTO ArrivedMessages SET ', newRowtoMsginInsert, function (error, results5) {
+                if (error) {
+                  callback(error, null)
+                  connection.end()
+                  return
+                }
+                callback(null, results5.affectedRows)
+                connection.end()
+                return
+              })
+            })
+          }
+          else {
+            callback(new Error('Exceed Apart'), null)
+            connection.end()
+            return
+          }
         }
-      }
+      })
     })
   }
 }
--- logic/subscribe/setSubscribeModelLogic.js
+++ logic/subscribe/setSubscribeModelLogic.js
@@ -1,72 +1,82 @@
 var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'adp',
-  database: 'FlieralSite'
-})
 
 module.exports = {
   setSubscriberModel: function (token, payload, callback) {
-    connection.connect()
-    connection.query('SELECT * FROM SubscribeToken WHERE Token =?', token, function (error, results) {
-      if (error) {
-        callback(error, null)
-        connection.end()
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'adp',
+      database: 'FlieralSite'
+    })
+    connection.connect(function (err) {
+      if (err) {
+        callback(err, null)
         return
       }
-      if (results[0].Token !== null && results[0].Counter < 5) {
-        var newCounter = results[0].Counter + 1
-        connection.query('UPDATE SubscribeToken SET Counter = ? WHERE Token = ?', [newCounter, token], function (error, results2) {
-          if (error) {
-            callback(error, null)
-            connection.end()
-            return
-          }
-          var newRowtoSubinUpdate = {
-            Email: payload.email,
-            STokenId: token
-          }
-          connection.query('INSERT INTO Subscribers SET ', newRowtoSubinUpdate, function (error, results3) {
+      connection.query('SELECT * FROM SubscribeToken WHERE Token = ?', token, function (error, results) {
+        if (error) {
+          callback(error, null)
+          connection.end()
+          return
+        }
+        if ((results.length != 0) && results[0].Token !== null && parseInt(results[0].Counter, 10) < 5) {
+          var newCounter = parseInt(results[0].Counter, 10) + 1
+          connection.query('UPDATE SubscribeToken SET Counter = ? WHERE Token = ?', [newCounter, token], function (error, results2) {
             if (error) {
               callback(error, null)
               connection.end()
               return
             }
-            callback(null, results3.affectedRows)
-            connection.end()
-            return
-          })
-        })
-      } else {
-        if (results[0].Token === null) {
-          var newRowtoSubToken = {
-            Token: token,
-            Counter: 1
-          }
-          connection.query('INSERT INTO SubscribeToken SET ', newRowtoSubToken, function (error, results4) {
-            if (error) {
-              callback(error, null)
-              connection.end()
-              return
-            }
-            var newRowtoSubinInsert = {
+            var newRowtoSubinUpdate = {
               Email: payload.email,
-              STokenId: results4.insertId
+              STokenId: token
             }
-            connection.query('INSERT INTO Subscribers SET ', newRowtoSubinInsert, function (error, results5) {
+            connection.query('INSERT INTO Subscribers SET ?', newRowtoSubinUpdate, function (error, results3) {
               if (error) {
                 callback(error, null)
                 connection.end()
                 return
               }
-              callback(null, results5.affectedRows)
+              callback(null, results3.affectedRows)
               connection.end()
               return
             })
           })
+        } else {
+          if (results.length == 0) {
+            var newRowtoSubToken = {
+              Token: token,
+              Counter: 1
+            }
+            connection.query('INSERT INTO SubscribeToken SET ?', newRowtoSubToken, function (error, results4) {
+              if (error) {
+                callback(error, null)
+                connection.end()
+                return
+              }
+              var newRowtoSubinInsert = {
+                Email: payload.email,
+                STokenId: results4.insertId
+              }
+              connection.query('INSERT INTO Subscribers SET ?', newRowtoSubinInsert, function (error, results5) {
+                if (error) {
+                  callback(error, null)
+                  connection.end()
+                  return
+                }
+                callback(null, results5.affectedRows)
+                connection.end()
+                return
+              })
+            })
+          }
+          else {
+            callback(new Error('Exceed Apart'), null)
+            connection.end()
+            return
+          }
         }
-      }
+      })
     })
   }
 }
